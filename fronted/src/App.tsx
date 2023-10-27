c04968761bc250a8946227e83f96823f2b2a7612
cd-cd-cd <1262877491@qq.com>
2023-10-28 00:56:18 +0800

    feat: sass

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='register' element={<Register/>}></Route>
+          <Route path='*' element={<Navigate to='/login' />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }

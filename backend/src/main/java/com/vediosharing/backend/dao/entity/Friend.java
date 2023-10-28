0fea3f9e16896514722965e570da53166e05c717
dengzuxuan <dengzuxuan20010404@126.com>
2023-10-28 20:47:30 +0800

    add error message

---
+++ backend/src/main/java/com/vediosharing/backend/dao/entity/Friend.java
@@ -0,0 +1,11 @@
+package com.vediosharing.backend.dao.entity;
+
+/**
+ * @ClassName Friend
+ * @Description TODO
+ * @Author Colin
+ * @Date 2023/10/28 20:40
+ * @Version 1.0
+ */
+public class Friend {
+}

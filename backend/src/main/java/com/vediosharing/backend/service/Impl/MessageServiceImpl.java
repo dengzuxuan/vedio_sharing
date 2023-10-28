2076a2da63ea785da56d16d71dcf64e4314c18c8
dengzuxuan <dengzuxuan20010404@126.com>
2023-10-28 13:52:16 +0800

    del yml file

--- backend/src/main/java/com/vediosharing/backend/service/Impl/MessageServiceImpl.java
+++ backend/src/main/java/com/vediosharing/backend/service/Impl/MessageServiceImpl.java
@@ -1,7 +1,13 @@
 package com.vediosharing.backend.service.Impl;
 
 import com.vediosharing.backend.core.constant.Result;
+import com.vediosharing.backend.dao.entity.Message;
+import com.vediosharing.backend.dao.mapper.MessageMapper;
 import com.vediosharing.backend.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @ClassName MessageServiceImpl
@@ -10,14 +16,31 @@ import com.vediosharing.backend.service.MessageService;
  * @Date 2023/10/28 2:07
  * @Version 1.0
  */
+@Service
 public class MessageServiceImpl implements MessageService {
+    @Autowired
+    private MessageMapper messageMapper;
+
     @Override
     public int getNotReadMessage(int userId) {
         return 0;
     }
 
     @Override
-    public Result addMessage(int Type, String message, int sendUserId) {
-        return null;
+    public void addMessage(int type, String message, int recvUserId, int sendUserId) {
+        Date now = new Date();
+        Message newMessage = new Message(
+                null,
+                message,
+                recvUserId,
+                sendUserId,
+                type,
+                0,
+                now,
+                now
+        );
+
+        messageMapper.insert(newMessage);
     }
+
 }
